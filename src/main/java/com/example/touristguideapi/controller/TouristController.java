f9c9476e219c63e1af7c617fe33b1241c51a3fa8
MetteMunch <meha0005@stud.kea.dk>
2024-09-08 17:06:01 +0200

    tilføjet et par links til andre statiske HTML sider og billeder, som dog ikke vises ret godt

--- src/main/java/com/example/touristguideapi/controller/TouristController.java
+++ src/main/java/com/example/touristguideapi/controller/TouristController.java
@@ -20,28 +20,28 @@ public class TouristController {
         this.ts = ts;
     }
 
-    @GetMapping //denne get Endpoint hentes, når der bare skrives /attractions i browseren
+    @GetMapping //denne get Endpoint hentes og metoden køres, når der bare skrives /attractions i browseren
     public ResponseEntity<List<TouristAttraction>> getAllAttractions(){
         List<TouristAttraction> getAllAttractions = ts.getListOfAttraction();
         return new ResponseEntity<>(getAllAttractions, HttpStatus.OK);
     }
 
-    @GetMapping("/{name}") //denne get Endpoint hentes, når der angives et name i browseren fx attractions/hærvejen
+    @GetMapping("/{name}") //denne get Endpoint hentes og metoden køres, når der angives et name i browseren fx attractions/hærvejen
     public ResponseEntity<String> getDescriptionByName(@PathVariable String name){
         return new ResponseEntity<String>(ts.getDescription(name),HttpStatus.OK);
     }
-    @PostMapping("/add") //denne post Endpoint hentes, når der skrives attractions/add i browseren
+    @PostMapping("/add") //denne post Endpoint hentes og metoden køres, når der skrives attractions/add i browseren
     public ResponseEntity<Void> addAttraction(@RequestParam String name, @RequestParam String description){
         ts.addAttraction(name,description);
         return new ResponseEntity<>(HttpStatus.OK);
     }
-    @PostMapping("/update") //denne post Endpoint hentes, når der skrives attraction/update
+    @PostMapping("/update") //denne post Endpoint hentes og metoden køres, når der skrives attractions/update
     public ResponseEntity<Void> updateAttraction(@RequestParam String name, @RequestParam String decription){
         ts.updateAttraction(name, decription);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping("/delete") //denne post Endpoint hentes, når der skrives attraction/delete
+    @PostMapping("/delete") //denne post Endpoint hentes og metoden køres, når der skrives attractions/delete
     public ResponseEntity<Void> deleteAttraction(@RequestParam String name){
         ts.deleteAttraction(name);
         return new ResponseEntity<>(HttpStatus.OK);
